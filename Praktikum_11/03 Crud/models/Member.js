9b005ef8fb6b1674e3e9fa75931875b011f811af
gabrielafel <70675316+gabrielafel@users.noreply.github.com>
2020-11-23 19:17:10 +0700

    Praktikum11

---
+++ Praktikum_11/03 Crud/models/Member.js
@@ -0,0 +1,8 @@
+  
+const mongoose = require("mongoose"),
+memberSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    credit: Number
+});
+module.exports = mongoose.model("Member", memberSchema);
